27d616042605a4bd224ec1523d4cadfbc03d3afa
timadigwe <timothyadigwe2019@gmail.com>
2023-11-11 06:49:58 +0100

    ts error fixed

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,7 +1,12 @@
+import { MagicUserMetadata } from "magic-sdk";
 import { createContext, Dispatch, SetStateAction } from "react";
 
 export type UserContextType = {
-  user: any; // Change 'any' to the appropriate type for the user data
+  user?: {
+    issuer?: string; // Add this line if issuer is part of the user object
+    // Other properties of the user object
+  } | null;
+  magicUserMetadata?: MagicUserMetadata;
 };
 
 export const UserContext = createContext<
--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,20 +1,18 @@
-import { Magic } from "@magic-sdk/admin";
 import { NextApiRequest, NextApiResponse } from "next";
+import { Magic } from "@magic-sdk/admin";
 
 // Initiating Magic instance for server-side methods
 const magic = new Magic(process.env.MAGIC_SECRET_KEY as string);
 
-export default async function login(req: NextApiRequest, res: NextApiResponse) {
+export default async function login(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
   try {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      throw new Error("Authorization header not found");
-    }
-
-    const didToken = authHeader.substring(7);
+    const didToken: string = req.headers.authorization?.substr(7) || "";
     await magic.token.validate(didToken);
     res.status(200).json({ authenticated: true });
   } catch (error: any) {
-    res.status(500).json({ error: error.message as string });
+    res.status(500).json({ error: error.message });
   }
 }
