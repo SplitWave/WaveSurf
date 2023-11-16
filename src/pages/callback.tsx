7aa4b21548c1c46f9580e465c6914bcf8f1cc0fe
timadigwe <timothyadigwe2019@gmail.com>
2023-11-16 03:51:29 +0100

    db added

--- src/pages/callback.tsx
+++ src/pages/callback.tsx
@@ -3,6 +3,9 @@ import Router, { useRouter } from "next/router";
 import { magic } from "../lib/magic";
 import { UserContext } from "../context/UserContext";
 import { ThreeDots } from "react-loader-spinner";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "@/lib/firebase";
+import { ref, set } from "firebase/database";
 
 const Callback = () => {
   const router = useRouter();
@@ -42,6 +45,16 @@ const Callback = () => {
       // Set the UserContext to the now logged in user
       let userMetadata = await magic?.user.getMetadata();
       console.log("userMetadata", userMetadata);
+      // try {
+      //   const docRef = await addDoc(collection(db, "users"), { userMetadata });
+      //   console.log("User data added to firestore with ID:", docRef.id);
+      // } catch (error) {
+      //   console.error("Error adding user data to Firestore:", error);
+      // }
+      set(ref(db, "users/"), {
+        userMetadata,
+      });
+
       setUser({ user: userMetadata });
       Router.push("/dashboard");
     }
